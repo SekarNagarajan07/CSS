05a5aaf48d8a9fa50503c9767346732bd6a06258
sekar N <sekarnagarajan1234@gmail.com>
2023-08-02 22:48:34 +0530

    js

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -20,3 +20,27 @@ const toastDetails = {
     text: "Info : This is a information toast.",
   },
 };
+
+const removeToast = (toast) => {
+  toast.classList.add("hide");
+  if (toast.timeoutId) clearTimeout(toast.timeoutId);
+  setTimeout(() => toast.remove(), 500);
+};
+
+const createToast = (id) => {
+  const { icon, text } = toastDetails[id];
+  const toast = document.createElement("li");
+  toast.className = `toast ${id}`;
+  toast.innerHTML = `<div class="column">
+  <i class="fa-solid ${icon}"></i>
+  <span>${text}</span>
+</div>
+<i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+
+  notifications.appendChild(toast);
+  toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+};
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => createToast(btn.id));
+});
